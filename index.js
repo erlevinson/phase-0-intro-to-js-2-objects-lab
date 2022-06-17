490983d596990ff2b51f06e50f487f4bdb7c13d2
Eric <ericlevinson3@gmail.com>
2022-06-17 17:31:03 -0500

    commit tree 90abc145f863df459b5e963a952e714205b9cd50

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Eric",
+    streeAdddress: "2233 West Medill Ave, Chicago, IL"
+}
+
+function updateEmployeeWithKeyAndValue (object, key, value){
+    return {
+        ...object,
+        [key]: value
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (object, key, value){
+    object[key] = value
+    return object
+}
+
+function deleteFromEmployeeByKey (object, key, value){
+    const newEmployee = {...object}
+    newEmployee[key] = value
+    return newEmployee
+    }
+
+function destructivelyDeleteFromEmployeeByKey  (object, key, value){
+    object[key] = value
+    return object
+}
